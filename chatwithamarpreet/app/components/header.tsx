ff24a407b9674d1b4f8f35fd43299d72992f0950
Amarpreet Bhatia <amarpreetbhatia@gmail.com>
2024-08-05 13:31:30 +0000

    created a chatbot using llamaindex typescript

---
+++ chatwithamarpreet/app/components/header.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (<>
+    <div>
+      <p>
+        Get started by&nbsp;
+        <b>Amarpreet: Let's Connect</b>
+      </p>
+      </div>
+      <div>
+      <p>
+        Discover the person behind the name. This chatbot is to know about Amarpreet,
+        Let's chat!
+      </p>
+    </div></>
+  );
+}
